1fde5a4a46bfc528bf8ccbc8f5f66d7da5bbc9e0
Petro <locator862@gmail.com>
2023-04-17 19:17:12 +0200

    added floorsCountInput

--- js/floors_buttons_config.js
+++ js/floors_buttons_config.js
@@ -6,8 +6,8 @@ if (floorsCount !== null) {
 
 document.querySelector('#generateFloors').addEventListener('click', function() {
   floorsCount = document.querySelector('#floorsCountInput').value;
-  if (floorsCount < 5 || floorsCount > 13) {
-    alert('Для найкращого візуального враження, будь ласка, введіть кількість поверхів у діапазоні від 5 до 13. Дякую за увагу до деталей!');
+  if (floorsCount < 5 || floorsCount > 10) {
+    alert('Для найкращого візуального враження, будь ласка, введіть кількість поверхів у діапазоні від 5 до 10. Дякую за увагу до деталей!');
     floorsInLocalStorage = 5;
     return;
   } 
