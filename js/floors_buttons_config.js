d5d42fed13a28ccf6be343ab56b4e8ea4fca594b
Petro <locator862@gmail.com>
2023-04-17 19:09:43 +0200

    added floorsCountInput

--- js/floors_buttons_config.js
+++ js/floors_buttons_config.js
@@ -1,5 +1,21 @@
+let floorsCount = localStorage.getItem('floorsCount');
+
+if (floorsCount !== null) {
+  document.querySelector('#floorsCountInput').value = floorsCount;
+}
+
+document.querySelector('#generateFloors').addEventListener('click', function() {
+  floorsCount = document.querySelector('#floorsCountInput').value;
+  if (floorsCount < 5 || floorsCount > 13) {
+    alert('Для найкращого візуального враження, будь ласка, введіть кількість поверхів у діапазоні від 5 до 13. Дякую за увагу до деталей!');
+    floorsInLocalStorage = 5;
+    return;
+  } 
+  localStorage.setItem('floorsCount', floorsCount);
+  floorsCount = '';
+  location.reload();
+});
+
 const configFloors = {
-  floorsCount : 5,
-  language : 'EN',
-  sound : 'url....'
+  floorsCount : floorsCount,
 }
